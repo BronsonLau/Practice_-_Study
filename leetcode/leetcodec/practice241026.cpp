c1ef2a6596b816fad03cf042b30ebb7252c1c2d5
keyfbruce <2316204262@qq.com>
2024-10-26 15:31:29 +0800

    24 10 26 动态规划 H

--- leetcode/leetcodec/practice241026.cpp
+++ leetcode/leetcodec/practice241026.cpp
@@ -1,3 +1,42 @@
+// 3181. 执行操作可获得的最大总奖励 II
+
+// 给你一个整数数组 rewardValues，长度为 n，代表奖励的值。
+
+// 最初，你的总奖励 x 为 0，所有下标都是 未标记 的。你可以执行以下操作 任意次 ：
+
+// 从区间 [0, n - 1] 中选择一个 未标记 的下标 i。
+// 如果 rewardValues[i] 大于 你当前的总奖励 x，则将 rewardValues[i] 加到 x 上（即 x = x + rewardValues[i]），并 标记 下标 i。
+// 以整数形式返回执行最优操作能够获得的 最大 总奖励。
+
+
+
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        int n = rewardValues.size();
+        sort(rewardValues.begin(), rewardValues.end());
+        if (n >= 2 && rewardValues[n - 2] == rewardValues[n - 1] - 1) {
+            return 2 * rewardValues[n - 1] - 1;
+        }
+        bitset<100000> f0, f1;
+        f0[0] = 1;
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j < rewardValues[i]) {
+                f1[j] = f0[j];
+                j++;
+            }
+            f0 |= f1 << rewardValues[i];
+        }
+        int res = 0;
+        for (int i = 0; i < f0.size(); i++) {
+            if (f0[i]) {
+                res = i;
+            }
+        }
+        return res;
+    }
+};
+
 // 位运算优化 + 动态规划
 // 记 rewardValues 的最大值为 m，因为最后一次操作前的总奖励一定小于等于 m−1，所以可获得的最大总奖励小于等于 2m−1。
 
@@ -61,33 +100,3 @@
 // 1
 // ​
 //   的低 x 位。因此我们可以使用位运算对动态规划进行优化，降低时间复杂度。
-
-
-
-class Solution {
-public:
-    int maxTotalReward(vector<int>& rewardValues) {
-        int n = rewardValues.size();
-        sort(rewardValues.begin(), rewardValues.end());
-        if (n >= 2 && rewardValues[n - 2] == rewardValues[n - 1] - 1) {
-            return 2 * rewardValues[n - 1] - 1;
-        }
-        bitset<100000> f0, f1;
-        f0[0] = 1;
-        for (int i = 0, j = 0; i < n; i++) {
-            while (j < rewardValues[i]) {
-                f1[j] = f0[j];
-                j++;
-            }
-            f0 |= f1 << rewardValues[i];
-        }
-        int res = 0;
-        for (int i = 0; i < f0.size(); i++) {
-            if (f0[i]) {
-                res = i;
-            }
-        }
-        return res;
-    }
-};
-

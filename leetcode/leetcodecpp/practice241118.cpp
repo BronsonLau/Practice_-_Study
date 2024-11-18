fc0ba4a9ca7cbc86686e31721cec8a91f99c7359
keyfbruce <keyfrankbruce@gmail.com>
2024-11-18 08:26:41 +0800

    24 11 18 循环遍历法

---
+++ leetcode/leetcodecpp/practice241118.cpp
@@ -0,0 +1,36 @@
+// 661. 图片平滑器
+// 图像平滑器 是大小为 3 x 3 的过滤器，用于对图像的每个单元格平滑处理，平滑处理后单元格的值为该单元格的平均灰度。
+
+// 每个单元格的  平均灰度 定义为：该单元格自身及其周围的 8 个单元格的平均值，结果需向下取整。（即，需要计算蓝色平滑器中 9 个单元格的平均值）。
+
+// 如果一个单元格周围存在单元格缺失的情况，则计算平均灰度时不考虑缺失的单元格（即，需要计算红色平滑器中 4 个单元格的平均值）。
+
+
+
+// 给你一个表示图像灰度的 m x n 整数矩阵 img ，返回对图像的每个单元格平滑处理后的图像 。
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size(), n = img[0].size();
+        vector<vector<int>> ret(m, vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = 0, sum = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (x >= 0 && x < m && y >= 0 && y < n) {
+                            num++;
+                            sum += img[x][y];
+                        }
+                    }
+                }
+                ret[i][j] = sum / num;
+            }
+        }
+        return ret;
+    }
+};
+
+// 方法：遍历每个单元格，计算周围单元格的平均值
+// 时间复杂度：O(m*n)
+// 空间复杂度：O(1)
